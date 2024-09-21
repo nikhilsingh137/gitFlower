b396acd81069a183a6c51c82052d2f7f07c5a7a2
nikhilsingh137 <nikhilsurvanshi137@gmail.com>
2024-09-21 17:43:17 +0530

    update

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,48 +4,43 @@ import ShopSection from "./component/ShopSection";
 import ProductDetail from "./component/ProductDetail";
 import ProductRoute from "./component/ProductRoute";
 
-const router = createHashRouter(
-  [
-    {
-      path: "/",
-      element: (
-        <>
-          <Homepage />
-        </>
-      ),
-    },
-    {
-      path: "/:userId",
-      element: (
-        <>
-          <ShopSection />
-        </>
-      ),
-    },
-    {
-      path: "/product/:userId",
-      element: (
-        <>
-          <ProductDetail />
-        </>
-      ),
-    },
-    {
-      path: "product/:userId/:userId",
-      element: (
-        <>
-          <ProductRoute />
-        </>
-      ),
-    },
-    {
-      path: "/about",
-      element: <>Hello</>,
-    },
-  ],
+const router = createHashRouter([
   {
-    basename: "/gitFlower",
-  }
-);
+    path: "/",
+    element: (
+      <>
+        <Homepage />
+      </>
+    ),
+  },
+  {
+    path: "/:userId",
+    element: (
+      <>
+        <ShopSection />
+      </>
+    ),
+  },
+  {
+    path: "/product/:userId",
+    element: (
+      <>
+        <ProductDetail />
+      </>
+    ),
+  },
+  {
+    path: "product/:userId/:userId",
+    element: (
+      <>
+        <ProductRoute />
+      </>
+    ),
+  },
+  {
+    path: "/about",
+    element: <>Hello</>,
+  },
+]);
 
 export default router;

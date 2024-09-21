90eb49dab8edca3660c957ad08ee75069cbc0fbb
nikhilsingh137 <nikhilsurvanshi137@gmail.com>
2024-09-21 16:54:36 +0530

    update

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,43 +4,48 @@ import ShopSection from "./component/ShopSection";
 import ProductDetail from "./component/ProductDetail";
 import ProductRoute from "./component/ProductRoute";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: (
+        <>
+          <Homepage />
+        </>
+      ),
+    },
+    {
+      path: "/:userId",
+      element: (
+        <>
+          <ShopSection />
+        </>
+      ),
+    },
+    {
+      path: "/product/:userId",
+      element: (
+        <>
+          <ProductDetail />
+        </>
+      ),
+    },
+    {
+      path: "product/:userId/:userId",
+      element: (
+        <>
+          <ProductRoute />
+        </>
+      ),
+    },
+    {
+      path: "/about",
+      element: <>Hello</>,
+    },
+  ],
   {
-    path: "/",
-    element: (
-      <>
-        <Homepage />
-      </>
-    ),
-  },
-  {
-    path: "/:userId",
-    element: (
-      <>
-        <ShopSection />
-      </>
-    ),
-  },
-  {
-    path: "/product/:userId",
-    element: (
-      <>
-        <ProductDetail />
-      </>
-    ),
-  },
-  {
-    path: "product/:userId/:userId",
-    element: (
-      <>
-        <ProductRoute />
-      </>
-    ),
-  },
-  {
-    path: "/about",
-    element: <>Hello</>,
-  },
-]);
+    basename: "/gitFlower",
+  }
+);
 
 export default router;

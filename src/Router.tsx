04c1e5eff40fa415ceddd16bf8b632e34af05314
nikhilsingh137 <nikhilsurvanshi137@gmail.com>
2024-09-21 17:39:13 +0530

    update

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import Homepage from "./page/Homepage";
 import ShopSection from "./component/ShopSection";
 import ProductDetail from "./component/ProductDetail";
 import ProductRoute from "./component/ProductRoute";
 
-const router = createBrowserRouter(
+const router = createHashRouter(
   [
     {
       path: "/",

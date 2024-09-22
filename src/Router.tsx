b427d0ea5fdc299d63d52e9ada2c937d95e5fca4
nikhilsingh137 <nikhilsurvanshi137@gmail.com>
2024-09-23 04:00:10 +0530

    update

--- src/Router.tsx
+++ src/Router.tsx
@@ -7,63 +7,69 @@ import ContactPage from "./page/ContactPage";
 import ContactGetData from "./component/ContactGetData";
 import AboutPage from "./page/AboutPage";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: (
+        <>
+          <Homepage />
+        </>
+      ),
+    },
+    {
+      path: "/:userId",
+      element: (
+        <>
+          <ShopSection />
+        </>
+      ),
+    },
+    {
+      path: "/product/:userId",
+      element: (
+        <>
+          <ProductDetail />
+        </>
+      ),
+    },
+    {
+      path: "product/:userId/:userId",
+      element: (
+        <>
+          <ProductRoute />
+        </>
+      ),
+    },
+    {
+      path: "/about",
+      element: (
+        <>
+          <AboutPage />
+        </>
+      ),
+    },
+    {
+      path: "/contact",
+      element: (
+        <>
+          <ContactPage />
+        </>
+      ),
+    },
+    {
+      path: "/formData",
+      element: (
+        <>
+          <ContactGetData />
+        </>
+      ),
+    },
+  ],
+
   {
-    path: "/",
-    element: (
-      <>
-        <Homepage />
-      </>
-    ),
-  },
-  {
-    path: "/:userId",
-    element: (
-      <>
-        <ShopSection />
-      </>
-    ),
-  },
-  {
-    path: "/product/:userId",
-    element: (
-      <>
-        <ProductDetail />
-      </>
-    ),
-  },
-  {
-    path: "product/:userId/:userId",
-    element: (
-      <>
-        <ProductRoute />
-      </>
-    ),
-  },
-  {
-    path: "/about",
-    element: (
-      <>
-        <AboutPage />
-      </>
-    ),
-  },
-  {
-    path: "/contact",
-    element: (
-      <>
-        <ContactPage />
-      </>
-    ),
-  },
-  {
-    path: "/formData",
-    element: (
-      <>
-        <ContactGetData />
-      </>
-    ),
-  },
-]);
+    basename: "/gitflower/",
+  }
+);
 
 export default router;
